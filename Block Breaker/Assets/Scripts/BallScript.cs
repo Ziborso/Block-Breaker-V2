58899cf155344a71218d161a3eb16abbb3257990
maciekrapacz <maciek.rapacz96@gmail.com>
2019-05-30 18:22:45 +0200

    Scripts for ball paddle

---
+++ Block Breaker/Assets/Scripts/BallScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D> ();
+
+        rb.AddForce (Vector2.up * 500);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
