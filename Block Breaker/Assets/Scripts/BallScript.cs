4af2ad0bef5dc6c37607129ca02559c8c8a1a6f0
maciekrapacz <maciek.rapacz96@gmail.com>
2019-06-02 16:43:38 +0200

    Added colider, lose lvl and coppy position from padle scripts.

--- Block Breaker/Assets/Scripts/BallScript.cs
+++ Block Breaker/Assets/Scripts/BallScript.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public bool inPlay;
+    public Transform paddle;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +20,17 @@ public class BallScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!inPlay)
+        {
+            transform.position = paddle.position;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Bottom"))
+        {
+            SceneManager.LoadScene("Lose");
+        }
     }
 }
